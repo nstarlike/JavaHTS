87776bf5f829e2e50be51442e009b9ebe1979973
nstarlike <46280877+nstarlike@users.noreply.github.com>
2023-05-25 13:14:29 +0900

    Add Spring Boot project

---
+++ src/main/java/nstarlike/ss/ServletInitializer.java
@@ -0,0 +1,13 @@
+package nstarlike.ss;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StockSystemApplication.class);
+	}
+
+}
---
+++ src/test/java/nstarlike/ss/StockSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package nstarlike.ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
